4a26a630addc13937b1e127d08e80f2eb8b9f803
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-11-28 17:44:30 +0100

    Setup relation types on startup

--- Source/Our.Umbraco.Nexu.Core/BootStrapper.cs
+++ Source/Our.Umbraco.Nexu.Core/BootStrapper.cs
@@ -6,6 +6,16 @@
     /// Bootstrapper to handle umbraco startup events
     /// </summary>
     internal class BootStrapper : ApplicationEventHandler
-    {       
+    {
+        /// <summary>
+        /// Overridable method to execute when the ApplicationContext is created and other static objects that require initialization have been setup
+        /// </summary>
+        /// <param name="umbracoApplication"></param>
+        /// <param name="applicationContext"></param>
+        protected override void ApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            // setup needed relation types
+            NexuService.Current.SetupRelationTypes();
+        }
     }
 }
--- Source/Our.Umbraco.Nexu.Core/NexuService.cs
+++ Source/Our.Umbraco.Nexu.Core/NexuService.cs
@@ -1,5 +1,12 @@
 ﻿namespace Our.Umbraco.Nexu.Core
 {
+    using System;
+
+    using Constants;
+
+    using global::Umbraco.Core.Models;
+    using global::Umbraco.Core.Services;
+
     /// <summary>
     /// Nexu service
     /// </summary>
@@ -8,13 +15,14 @@
         /// <summary>
         /// Internal service instance
         /// </summary>
-        private static NexuService service;
+        private static NexuService service;      
 
         /// <summary>
         /// Prevents a default instance of the <see cref="NexuService"/> class from being created.
         /// </summary>
         private NexuService()
         {
+            this.RelationService = global::Umbraco.Core.ApplicationContext.Current.Services.RelationService;
             service = this;
         }
 
@@ -22,5 +30,68 @@
         /// The current nexu service instance
         /// </summary>
         public static NexuService Current => service ?? new NexuService();
+
+        /// <summary>
+        /// Gets or sets the relation service.
+        /// </summary>
+        private IRelationService RelationService { get; set; }
+
+        /// <summary>
+        /// Sets up the needed the relation types
+        /// </summary>
+        internal void SetupRelationTypes()
+        {
+            this.SetupDocumentToDocumentRelationType();
+            this.SetupDocumentToMediaRelationType();
+        }
+
+        /// <summary>
+        /// Sets up document to document relation type.
+        /// </summary>
+        private void SetupDocumentToDocumentRelationType()
+        {
+            if (this.RelationService.GetRelationTypeByAlias(RelationTypes.DocumentToDocumentAlias) != null)
+            {
+                return;
+            }
+
+            this.CreateRelationType(RelationTypes.DocumentToDocumentAlias, RelationTypes.DocumentToDocumentName, new Guid(global::Umbraco.Core.Constants.ObjectTypes.Document));            
+        }
+
+        /// <summary>
+        /// Sets up the document to media relation type.
+        /// </summary>
+        private void SetupDocumentToMediaRelationType()
+        {
+            if (this.RelationService.GetRelationTypeByAlias(RelationTypes.DocumentToMediaAlias) != null)
+            {
+                return;
+            }
+
+            this.CreateRelationType(RelationTypes.DocumentToMediaAlias, RelationTypes.DocumentToMediaName, new Guid(global::Umbraco.Core.Constants.ObjectTypes.Media));
+        }
+
+        /// <summary>
+        /// Creates a relation type.
+        /// </summary>
+        /// <param name="alias">
+        /// The alias of the relation type
+        /// </param>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <param name="childObjectType">
+        /// The child object type.
+        /// </param>
+        private void CreateRelationType(string alias, string name, Guid childObjectType)
+        {
+            var relationType = new RelationType(
+                                   childObjectType,
+                                   new Guid(global::Umbraco.Core.Constants.ObjectTypes.Document),
+                                   alias,
+                                   name);
+
+            this.RelationService.Save(relationType);
+        }
     }
 }
