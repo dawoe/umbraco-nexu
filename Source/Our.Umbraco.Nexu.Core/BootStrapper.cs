1a4656124dd77cdff8408981c9dfe07752de1f6a
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-11-28 08:40:15 +0100

    Added boot strapper

---
+++ Source/Our.Umbraco.Nexu.Core/BootStrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Our.Umbraco.Nexu.Core
+{
+    using global::Umbraco.Core;
+
+    /// <summary>
+    /// Bootstrapper to handle umbraco startup events
+    /// </summary>
+    internal class BootStrapper : ApplicationEventHandler
+    {        
+    }
+}
