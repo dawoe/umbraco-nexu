8af8914825849c5b70b89133e884de91629dd10d
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-11-30 08:23:54 +0100

    Added property parse interface

---
+++ Source/Our.Umbraco.Nexu.Core/Interfaces/IPropertyParser.cs
@@ -0,0 +1,24 @@
+﻿namespace Our.Umbraco.Nexu.Core.Interfaces
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Property parser interface
+    /// </summary>
+    public interface IPropertyParser
+    {
+        /// <summary>
+        /// Gets the property editor alias for which this parser is for
+        /// </summary>
+        string IsParserFor { get; }
+
+        /// <summary>
+        /// Get the linked entities from the property editor data
+        /// </summary>
+        /// <param name="value">The property value to parse</param>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        IEnumerable<ILinkedEntity> GetLinkedEntities(object value);
+    }
+}
