56623a9fd30b129d483568d3463ac5f20cd79168
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-11-30 08:11:19 +0100

    Added linked entity interface

---
+++ Source/Our.Umbraco.Nexu.Core/Enums/LinkedEntityType.cs
@@ -0,0 +1,11 @@
+﻿namespace Our.Umbraco.Nexu.Core.Enums
+{
+    /// <summary>
+    /// Linked entity type enum
+    /// </summary>
+    public enum LinkedEntityType
+    {
+        Document,
+        Media
+    }
+}
