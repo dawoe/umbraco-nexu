a8021f03d62d7f56eb112b05f4618c17cb1cbc16
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-01-10 07:57:38 +0100

    Added basic property parser

---
+++ Source/Our.Umbraco.Nexu.Parsers/ContentPickerParser.cs
@@ -0,0 +1,15 @@
+﻿namespace Our.Umbraco.Nexu.Parsers
+{
+    using System.Collections.Generic;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+    public class ContentPickerParser : IPropertyParser
+    {
+        public string IsParserFor { get; }
+
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(object value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
