75cd363a74a9b568a58b68c9bdca8cb2bc731e89
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-02-22 08:10:22 +0100

    Added linked media entity model

---
+++ Source/Our.Umbraco.Nexu.Core/Models/LinkedMediaEntity.cs
@@ -0,0 +1,27 @@
+﻿namespace Our.Umbraco.Nexu.Core.Models
+{
+    using Our.Umbraco.Nexu.Core.Enums;
+    using Our.Umbraco.Nexu.Core.Interfaces;
+    
+    /// <summary>
+    /// The linked media entity.
+    /// </summary>
+    public class LinkedMediaEntity : ILinkedEntity
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets the linked entity type.
+        /// </summary>
+        public LinkedEntityType LinkedEntityType
+        {
+            get
+            {
+                return LinkedEntityType.Media;
+            }
+        }
+    }
+}
