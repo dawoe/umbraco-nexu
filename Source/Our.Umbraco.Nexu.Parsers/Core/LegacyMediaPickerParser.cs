33a9dc6cef3ea54b9781b1fd08a34027aab24aee
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-02-27 08:48:17 +0100

    Added legacy mediapicker parser

---
+++ Source/Our.Umbraco.Nexu.Parsers/Core/LegacyMediaPickerParser.cs
@@ -0,0 +1,24 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Core
+{
+    using System.Collections.Generic;
+
+    using global::Umbraco.Core.Models;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+
+    /// <summary>
+    /// The legacy media picker parser.
+    /// </summary>
+    public class LegacyMediaPickerParser : IPropertyParser
+    {
+        public bool IsParserFor(PropertyType property)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(Property property)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
