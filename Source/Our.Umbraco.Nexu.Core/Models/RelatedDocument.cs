dec61362ac5489b0cc8ab69fafd8e2d2da0adb09
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-01 08:31:18 +0100

    Added related document model

---
+++ Source/Our.Umbraco.Nexu.Core/Models/RelatedDocument.cs
@@ -0,0 +1,33 @@
+﻿namespace Our.Umbraco.Nexu.Core.Models
+{
+    /// <summary>
+    /// The related document.
+    /// </summary>
+    internal class RelatedDocument
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the icon.
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether published.
+        /// </summary>
+        public bool Published { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether trashed.
+        /// </summary>
+        public bool Trashed { get; set; }
+    }
+}
