3c00aeac48044aeeec00b4fa5ee27cdefce01cb7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-02 10:06:04 +0100

    Added interceptor for content delete dialog

---
+++ Source/Our.Umbraco.Nexu.Core/Client/interceptor.js
@@ -0,0 +1,20 @@
+﻿angular.module('umbraco.services').config([
+   '$httpProvider',
+   function ($httpProvider) {
+
+       $httpProvider.interceptors.push(function ($q) {
+           return {
+               'request': function (request) {
+
+                   // Redirect any requests the built in content delete to our custom delete
+                   if (request.url === "views/content/delete.html") {
+                       request.url = '/App_Plugins/Nexu/views/content-delete.html';
+                   }
+                       
+
+                   return request || $q.when(request);
+               }
+           };
+       });
+
+   }]);
