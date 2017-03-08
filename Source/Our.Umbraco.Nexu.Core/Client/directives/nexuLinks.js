0a08b88688283fa4cd696b26f62fd65f8a8e0445
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-08 08:11:18 +0100

    Created directive for showing linked entities

---
+++ Source/Our.Umbraco.Nexu.Core/Client/directives/nexuLinks.js
@@ -0,0 +1,16 @@
+﻿angular.module("umbraco.directives")
+    .directive('nexuLinks', function () {
+        return {
+            restrict: "E",    // restrict to an element
+            replace: true,   // replace the html element with the template
+            transclude : true,
+            templateUrl: '/App_Plugins/Nexu/views/nexu-links.html',
+            scope: {
+                links: '=',
+                title : '@'
+            },
+            link: function (scope, element, attrs, ctrl) {
+                
+            }
+        };
+    });
