416d037245857984839de4bc70427bf0dc02d890
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-10 09:24:02 +0100

    Added parser for nested content

---
+++ Source/Our.Umbraco.Nexu.Parsers/Community/NestedContentParser.cs
@@ -0,0 +1,42 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Community
+{
+    using System.Collections.Generic;
+
+    using global::Umbraco.Core.Models;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+
+    /// <summary>
+    /// The nested content parser.
+    /// </summary>
+    public class NestedContentParser : IPropertyParser
+    {
+        /// <summary>
+        /// Check if it's a parser for a data type definition
+        /// </summary>
+        /// <param name="dataTypeDefinition">
+        /// The data type definition.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the linked entites from the property value
+        /// </summary>
+        /// <param name="propertyValue">
+        /// The property value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(object propertyValue)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
