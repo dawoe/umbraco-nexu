486cd5cb69c4a8f99b4fc27ec81d656b27c109f9
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-10 09:28:39 +0100

    Added unit test for nested content parser is parser for method

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/Community/NestedContentParserTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.Community
+{
+    using global::Umbraco.Core.Models;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.Community;
+
+    /// <summary>
+    /// The nested content parser tests.
+    /// </summary>
+    [TestFixture]
+    public class NestedContentParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CommunityParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("Our.Umbraco.NestedContent");
+
+            var parser = new NestedContentParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CommunityParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new NestedContentParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
