a7884ff45d330a971030bbed09ddcd50fcd79c06
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-11 15:38:16 +0100

    Added tests for getting linked entites method of nested content parser

--- Source/Our.Umbraco.Nexu.Parsers.Tests/Community/NestedContentParserTests.cs
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/Community/NestedContentParserTests.cs
@@ -1,9 +1,12 @@
 ﻿namespace Our.Umbraco.Nexu.Parsers.Tests.Community
 {
+    using System.Linq;
+
     using global::Umbraco.Core.Models;
 
     using NUnit.Framework;
 
+    using Our.Umbraco.Nexu.Core.Enums;
     using Our.Umbraco.Nexu.Parsers.Community;
 
     /// <summary>
@@ -50,6 +53,49 @@
 
             // verify
             Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// The test get linked entities with empty value.
+        /// </summary>
+        [Test]
+        public void TestGetLinkedEntitiesWithEmptyValue()
+        {
+            // arrange
+            var parser = new NestedContentParser();
+
+            object propValue = null;
+
+            // act
+            var result = parser.GetLinkedEntities(propValue);
+
+            // verify
+            Assert.IsNotNull(result);
+            var entities = result.ToList();
+            Assert.AreEqual(0, entities.Count());
+        }
+
+        /// <summary>
+        /// The test get linked entities with value.
+        /// </summary>
+        [Test]
+        public void TestGetLinkedEntitiesWithValue()
+        {
+            // arrange
+            var parser = new NestedContentParser();
+
+            string propValue = "[{\"name\":\"Item 1\",\"ncContentTypeAlias\":\"NCContentPicker\",\"picker\":\"1072\"},{\"name\":\"Item 2\",\"ncContentTypeAlias\":\"NCMediaPicker\",\"picker\":\"1081\"}]";
+
+            // act
+            var result = parser.GetLinkedEntities(propValue);
+
+            // verify
+            Assert.IsNotNull(result);
+            var entities = result.ToList();
+            Assert.AreEqual(2, entities.Count());
+
+            Assert.IsTrue(entities.Exists(x => x.LinkedEntityType == LinkedEntityType.Document && x.Id == 1072));
+            Assert.IsTrue(entities.Exists(x => x.LinkedEntityType == LinkedEntityType.Media && x.Id == 1081));
         }
     }
 }
