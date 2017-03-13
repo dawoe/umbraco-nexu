24bfc2e35eb0535d11c1c2ebc12045db31bf82aa
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-13 22:25:09 +0100

    Added unit tests for is parser for method of multiple media picker

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/Core/MultipleMediaPickerParserTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.Core
+{
+    using global::Umbraco.Core.Models;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.Core;
+
+    /// <summary>
+    /// The media picker parser tests.
+    /// </summary>
+    [TestFixture]
+    public class MultipleMediaPickerParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CoreParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition(global::Umbraco.Core.Constants.PropertyEditors.MultipleMediaPickerAlias);
+
+            var parser = new MultipleMediaPickerParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CoreParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new MultipleMediaPickerParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
