9a9aa4f18916bbcd84ff8979e37c071de9eb1b0f
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-14 08:18:45 +0100

    Added unit tests for IsParserFor method of RelatedLinksParser

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/Core/RelatedLinkParserTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.Core
+{
+    using global::Umbraco.Core.Models;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.Core;
+
+    /// <summary>
+    /// The related link parser tests.
+    /// </summary>
+    [TestFixture]
+    public class RelatedLinkParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CoreParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition(global::Umbraco.Core.Constants.PropertyEditors.RelatedLinksAlias);
+
+            var parser = new RelatedLinksParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CoreParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new RelatedLinksParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
