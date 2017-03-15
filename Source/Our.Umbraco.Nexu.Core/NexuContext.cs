78f2e5ba972247747365235e84c4a3fee532ce06
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-15 08:24:16 +0100

    Created nexu context

---
+++ Source/Our.Umbraco.Nexu.Core/NexuContext.cs
@@ -0,0 +1,32 @@
+﻿namespace Our.Umbraco.Nexu.Core
+{
+    /// <summary>
+    /// The nexu context.
+    /// </summary>
+    public class NexuContext
+    {
+        /// <summary>
+        /// The current instance.
+        /// </summary>
+        private static NexuContext instance;
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="NexuContext"/> class from being created.
+        /// </summary>
+        private NexuContext()
+        {
+            this.IsProcessing = false;
+            instance = this;
+        }
+
+        /// <summary>
+        /// Gets the current context
+        /// </summary>
+        public static NexuContext Current => instance ?? new NexuContext();
+
+        /// <summary>
+        /// Gets or sets a value indicating whether is processing.
+        /// </summary>
+        public bool IsProcessing { get; set; }
+    }
+}
