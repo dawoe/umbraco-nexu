e181cb6bd2a79a65e958c475af4c58ce6dd0480f
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-15 08:41:16 +0100

    Added function to angular nexu resource for getting current rebuild status

--- Source/Our.Umbraco.Nexu.Core/Client/resource.js
+++ Source/Our.Umbraco.Nexu.Core/Client/resource.js
@@ -3,6 +3,9 @@
             return {
                 getIncomingLinks: function (id) {
                     return $http.get(Umbraco.Sys.ServerVariables.Nexu.GetIncomingLinks + "?contentId=" + id);
+                },
+                getRebuildStatus : function() {
+                    return $http.get(Umbraco.Sys.ServerVariables.Nexu.GetRebuildStatus);
                 }
             };
         });
