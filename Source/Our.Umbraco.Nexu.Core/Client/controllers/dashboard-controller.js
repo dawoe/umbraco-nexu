d825458eb24d2898e756a874a383bb172c9232f8
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-17 11:26:58 +0100

    Implemented timeout function to query rebuild status

--- Source/Our.Umbraco.Nexu.Core/Client/controllers/dashboard-controller.js
+++ Source/Our.Umbraco.Nexu.Core/Client/controllers/dashboard-controller.js
@@ -1,28 +1,31 @@
 ﻿angular.module('umbraco').controller('Our.Umbraco.Nexu.DashboardController',
-    ['$scope', 'Our.Umbraco.Nexu.Resource', function ($scope, nexuResource) {
+    ['$scope', 'Our.Umbraco.Nexu.Resource', '$timeout', function ($scope, nexuResource, $timeout) {
         $scope.isLoading = true;
         $scope.RebuildStatus = {
             IsProcessing: true,
             ItemName: '',
             ItemsProcessed : 0
-        };
+        };        
 
         $scope.getRebuildStatus = function() {
             nexuResource.getRebuildStatus()
                 .then(function(result) {
                     $scope.isLoading = false;
                     $scope.RebuildStatus = result.data;
+
+                    if ($scope.RebuildStatus.IsProcessing) {
+                        $timeout(function() { $scope.getRebuildStatus() }, 1000, true);
+                    } 
                 });
         };
 
-        $scope.rebuild = function() {
-            $scope.RebuildStatus.IsProcessing = true;
-
-
+        $scope.rebuild = function() {           
             nexuResource.rebuild(-1)
                 .then(function(result) {
                     $scope.getRebuildStatus();
                 });
+
+            $timeout(function () { $scope.getRebuildStatus() }, 500, true);
         };
 
         $scope.getRebuildStatus();
