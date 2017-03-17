a84ac5bedbb06c935a71940e60c0ca28669cc1e1
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-17 17:41:12 +0100

    Added parser for Vorto propety editors

---
+++ Source/Our.Umbraco.Nexu.Parsers/Community/VortoParser.cs
@@ -0,0 +1,44 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Community
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using global::Umbraco.Core.Models;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+
+    /// <summary>
+    /// The vorto parser.
+    /// </summary>
+    public class VortoParser : IPropertyParser
+    {
+        /// <summary>
+        /// Check if it's a parser for a data type definition
+        /// </summary>
+        /// <param name="dataTypeDefinition">
+        /// The data type definition.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
+        {
+            // Our.Umbraco.Vorto
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the linked entites from the property value
+        /// </summary>
+        /// <param name="propertyValue">
+        /// The property value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(object propertyValue)
+        {
+            return Enumerable.Empty<ILinkedEntity>();
+        }
+    }
+}
