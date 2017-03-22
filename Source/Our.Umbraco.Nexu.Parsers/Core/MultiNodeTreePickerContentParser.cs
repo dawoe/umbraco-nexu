7bae8daaf70dc389ac6054ed589700e8df2f7f82
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-22 16:46:59 +0100

    Added multinode tree picker parser for content

---
+++ Source/Our.Umbraco.Nexu.Parsers/Core/MultiNodeTreePickerContentParser.cs
@@ -0,0 +1,24 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Core
+{
+    using System.Collections.Generic;
+
+    using global::Umbraco.Core.Models;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+
+    /// <summary>
+    /// The multi node tree picker content parser.
+    /// </summary>
+    public class MultiNodeTreePickerContentParser : IPropertyParser
+    {
+        public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(object propertyValue)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
