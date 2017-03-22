07ae6a2e96d584dd045cdddfa5634394a992a0eb
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-22 17:02:55 +0100

    Added unit tests for Is parser for of multinode treepicker content parser

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/Core/MultiNodeTreePickerContentParserTests.cs
@@ -0,0 +1,113 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.Core
+{
+    using System.Collections.Generic;
+
+    using global::Umbraco.Core.Models;
+    using global::Umbraco.Core.Services;
+
+    using Moq;
+
+    using Newtonsoft.Json.Linq;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.Community;
+    using Our.Umbraco.Nexu.Parsers.Core;
+
+    /// <summary>
+    /// The multi node tree picker content parser tests.
+    /// </summary>
+    [TestFixture]
+    public class MultiNodeTreePickerContentParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CoreParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeServiceMock = new Mock<IDataTypeService>();
+
+            var dataTypeDefinition =
+                new DataTypeDefinition(global::Umbraco.Core.Constants.PropertyEditors.MultiNodeTreePickerAlias)
+                    {
+                        Id = 1
+                    };
+
+            var prevalue = new JObject(new JProperty("type", "content"));
+
+            dataTypeServiceMock.Setup(x => x.GetPreValuesCollectionByDataTypeId(dataTypeDefinition.Id))
+                .Returns(
+                    new PreValueCollection(
+                        new Dictionary<string, PreValue> { { "startNode", new PreValue(prevalue.ToString()) } }));
+
+            var parser = new MultiNodeTreePickerContentParser(dataTypeServiceMock.Object);
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            dataTypeServiceMock.Verify(x => x.GetPreValuesCollectionByDataTypeId(dataTypeDefinition.Id), Times.Once);
+
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for valid data type with invalid prevalue.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CoreParsers")]
+        public void TestIsParserForValidDataTypeWithInvalidPrevalue()
+        {
+            // arrange
+            var dataTypeServiceMock = new Mock<IDataTypeService>();
+
+            var dataTypeDefinition =
+                new DataTypeDefinition(global::Umbraco.Core.Constants.PropertyEditors.MultiNodeTreePickerAlias)
+                {
+                    Id = 1
+                };
+
+            var prevalue = new JObject(new JProperty("type", "media"));
+
+            dataTypeServiceMock.Setup(x => x.GetPreValuesCollectionByDataTypeId(dataTypeDefinition.Id))
+                .Returns(
+                    new PreValueCollection(
+                        new Dictionary<string, PreValue> { { "startNode", new PreValue(prevalue.ToString()) } }));
+
+            var parser = new MultiNodeTreePickerContentParser(dataTypeServiceMock.Object);
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            dataTypeServiceMock.Verify(x => x.GetPreValuesCollectionByDataTypeId(dataTypeDefinition.Id), Times.Once);
+
+            Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("Parsers")]
+        [Category("CoreParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new MultiNodeTreePickerContentParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
--- Source/Our.Umbraco.Nexu.Parsers/Core/MultiNodeTreePickerContentParser.cs
+++ Source/Our.Umbraco.Nexu.Parsers/Core/MultiNodeTreePickerContentParser.cs
@@ -2,15 +2,43 @@
 {
     using System.Collections.Generic;
 
+    using global::Umbraco.Core;
     using global::Umbraco.Core.Models;
+    using global::Umbraco.Core.Services;
 
     using Our.Umbraco.Nexu.Core.Interfaces;
+    using Our.Umbraco.Nexu.Parsers.Community;
 
     /// <summary>
     /// The multi node tree picker content parser.
     /// </summary>
     public class MultiNodeTreePickerContentParser : IPropertyParser
     {
+
+        /// <summary>
+        /// The data type service.
+        /// </summary>
+        private readonly IDataTypeService dataTypeService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiNodeTreePickerContentParser"/> class.
+        /// </summary>
+        public MultiNodeTreePickerContentParser()
+        {
+            this.dataTypeService = ApplicationContext.Current.Services.DataTypeService;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiNodeTreePickerContentParser"/> class.
+        /// </summary>
+        /// <param name="dataTypeService">
+        /// The data type service.
+        /// </param>
+        public MultiNodeTreePickerContentParser(IDataTypeService dataTypeService)
+        {
+            this.dataTypeService = dataTypeService;
+        }
+
         public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
         {
             throw new System.NotImplementedException();
