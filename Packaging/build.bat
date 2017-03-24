b86816f8432d8ac7c7c816f7c6bc42999ca453c3
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-24 09:35:58 +0100

    Finalized packaging

--- Packaging/build.bat
+++ Packaging/build.bat
@@ -1,2 +1,2 @@
-Call ..\Sources\.nuget\Nuget.exe restore ..\Sources\Our.Umbraco.LoadBalancingDashboard.sln
+Call Tools\Nuget.exe restore ..\Source\Our.Umbraco.Nexu.sln
 %windir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe build.xml
