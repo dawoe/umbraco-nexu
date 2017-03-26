9209e50f4e8ec510bb7109c2e1801aabea6671a1
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-26 16:42:06 +0200

    Added unit tests for GetLinkedEntities of MediaGridParser

--- Source/Our.Umbraco.Nexu.Parsers.Tests/GridEditorParsers/Core/MediaGridEditorParserTests.cs
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/GridEditorParsers/Core/MediaGridEditorParserTests.cs
@@ -1,7 +1,10 @@
 ﻿namespace Our.Umbraco.Nexu.Parsers.Tests.GridEditorParsers.Core
 {
+    using System.Linq;
+
     using NUnit.Framework;
 
+    using Our.Umbraco.Nexu.Core.Enums;
     using Our.Umbraco.Nexu.Parsers.GridEditorParsers.Core;
 
     /// <summary>
@@ -44,6 +47,57 @@
 
             // verify
             Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// The test get linked entities with empty value.
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestGetLinkedEntitiesWithEmptyValue()
+        {
+            // arrange            
+            var parser = new MediaGridEditorParser();
+
+            // act
+            var result = parser.GetLinkedEntities(null);
+
+            // verify
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(0, result.Count());
+        }
+
+        /// <summary>
+        /// The test get linked entities.
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestGetLinkedEntities()
+        {
+            // arrange            
+            var parser = new MediaGridEditorParser();
+
+            string value = @" {
+                    ""focalPoint"": {
+                      ""left"": 0.4825,
+                      ""top"": 0.6067415730337079
+                    },
+                    ""id"": 1119,
+                    ""image"": ""/media/1037/9015601712_72e44263e4_b.jpg""
+                  }";
+
+            // act
+            var result = parser.GetLinkedEntities(value).ToList();
+
+            // verify
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(1, result.Count());
+
+            Assert.IsTrue(result.Any(x => x.LinkedEntityType == LinkedEntityType.Media && x.Id == 1119));
         }
     }
 }
