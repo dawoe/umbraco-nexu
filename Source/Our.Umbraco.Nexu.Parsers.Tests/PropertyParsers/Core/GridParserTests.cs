805d56a1450ae03b3f73866a8d54405cc5b2b4c7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-26 18:00:45 +0200

    Added unit tests for IsParserFor method of GridParser

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/PropertyParsers/Core/GridParserTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.PropertyParsers.Core
+{
+    using global::Umbraco.Core.Models;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.PropertyParsers.Core;
+
+    /// <summary>
+    /// The grid parser tests.
+    /// </summary>
+    [TestFixture]
+    public class GridParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CorePropertyParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition(global::Umbraco.Core.Constants.PropertyEditors.GridAlias);
+
+            var parser = new GridParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CorePropertyParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new GridParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
