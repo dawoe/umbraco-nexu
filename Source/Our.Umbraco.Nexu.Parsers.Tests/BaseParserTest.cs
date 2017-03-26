22d58eaa2431b3da50581d412fcefea4af655fc1
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-26 16:35:16 +0200

    Added unit tests for IsParser for of MediaGridEditorParser

--- Source/Our.Umbraco.Nexu.Parsers.Tests/BaseParserTest.cs
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/BaseParserTest.cs
@@ -10,6 +10,7 @@
     using NUnit.Framework;
 
     using Our.Umbraco.Nexu.Core.Interfaces;
+    using Our.Umbraco.Nexu.Core.ObjectResolution;
     using Our.Umbraco.Nexu.Resolvers;
 
     /// <summary>
@@ -31,14 +32,24 @@
         protected override void FreezeResolution()
         {
             var assembly = Assembly.Load("Our.Umbraco.Nexu.Parsers");
-            var parsers =
+            var propertyParsers =
                 TypeFinder.FindClassesOfType<IPropertyParser>(new List<Assembly> { assembly }).ToList();
 
-            // set up pattern model resolver
+            var gridEditorParsers =
+               TypeFinder.FindClassesOfType<IGridEditorParser>(new List<Assembly> { assembly }).ToList();
+
+            // set up property  parser resolver
             PropertyParserResolver.Current = new PropertyParserResolver(
                 new ActivatorServiceProvider(),
                 this.Logger,
-                parsers);
+                propertyParsers);
+
+            // setup grid editor parser resolver
+            GridEditorParserResolver.Current = new GridEditorParserResolver(
+                new ActivatorServiceProvider(),
+                this.Logger,
+                gridEditorParsers);
+
 
             base.FreezeResolution();
         }
---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/GridEditorParsers/Core/MediaGridEditorParserTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.GridEditorParsers.Core
+{
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.GridEditorParsers.Core;
+
+    /// <summary>
+    /// The media grid editor parser tests.
+    /// </summary>
+    [TestFixture]
+    public class MediaGridEditorParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid view
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestIsParserForValidView()
+        {
+            // arrange            
+            var parser = new MediaGridEditorParser();
+
+            // act
+            var result = parser.IsParserFor("media");
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestIsParserForInValidView()
+        {
+            // arrange            
+            var parser = new MediaGridEditorParser();
+
+            // act
+            var result = parser.IsParserFor("foo");
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
