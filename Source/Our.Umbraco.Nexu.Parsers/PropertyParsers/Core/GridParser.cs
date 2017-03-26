5c5fd0fc39272db248d7d3c0d08ede9dc7016b52
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-26 16:50:09 +0200

    Added Grid Property Parser

---
+++ Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Core/GridParser.cs
@@ -0,0 +1,24 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.PropertyParsers.Core
+{
+    using System.Collections.Generic;
+
+    using global::Umbraco.Core.Models;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+
+    /// <summary>
+    /// The grid parser.
+    /// </summary>
+    public class GridParser : IPropertyParser
+    {
+        public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(object propertyValue)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
