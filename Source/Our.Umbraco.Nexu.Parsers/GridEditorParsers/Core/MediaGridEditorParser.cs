119fbfb39262fbeb52772946fac56e28da83fe40
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-26 16:22:00 +0200

    Added media grid editor parser

---
+++ Source/Our.Umbraco.Nexu.Parsers/GridEditorParsers/Core/MediaGridEditorParser.cs
@@ -0,0 +1,22 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.GridEditorParsers.Core
+{
+    using System.Collections.Generic;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+
+    /// <summary>
+    /// The media grid editor parser.
+    /// </summary>
+    public class MediaGridEditorParser : IGridEditorParser
+    {
+        public bool IsParserFor(string editorview)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(string value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
