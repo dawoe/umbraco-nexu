bdbe9253bc31cead371c29cdace554cfd481647a
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-26 15:17:25 +0200

    Added interface for grid editor parsers

---
+++ Source/Our.Umbraco.Nexu.Core/Interfaces/IGridEditorParser.cs
@@ -0,0 +1,32 @@
+﻿namespace Our.Umbraco.Nexu.Core.Interfaces
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The GridEditorParser interface.
+    /// </summary>
+    public interface IGridEditorParser
+    {
+        /// <summary>
+        /// Check if it is a parser for a speficic editor
+        /// </summary>
+        /// <param name="editorview">
+        /// The editor view alias as defined in grid config
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        bool IsParserFor(string editorview);
+
+        /// <summary>
+        /// Gets the linked entities for this editor
+        /// </summary>
+        /// <param name="value">
+        /// The grid editor value
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        IEnumerable<ILinkedEntity> GetLinkedEntities(string value);
+    }
+}
