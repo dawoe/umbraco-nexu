317334f7e12cb684457516101bc8233dc5405843
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-27 08:57:39 +0200

    Added unit test for IsParserFor method of RteGridEditorParser

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/GridEditorParsers/Core/RteGridEditorParserTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.GridEditorParsers.Core
+{
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.GridEditorParsers.Core;
+
+    /// <summary>
+    /// The rte grid editor parser tests.
+    /// </summary>
+    [TestFixture]
+    public class RteGridEditorParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid view
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestIsParserForValidView()
+        {
+            // arrange            
+            var parser = new RteGridEditorParser();
+
+            // act
+            var result = parser.IsParserFor("rte");
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestIsParserForInValidView()
+        {
+            // arrange            
+            var parser = new RteGridEditorParser();
+
+            // act
+            var result = parser.IsParserFor("foo");
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
