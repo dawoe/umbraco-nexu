ddfd6a63126c45283f6ec03eb7f689a908e8ac61
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-29 16:52:20 +0200

    Added parser for doctype grid editor

---
+++ Source/Our.Umbraco.Nexu.Parsers/GridEditorParsers/Community/DocTypeGridEditorParser.cs
@@ -0,0 +1,40 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.GridEditorParsers.Community
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+    /// <summary>
+    /// The doc type grid editor parser.
+    /// </summary>
+    public class DocTypeGridEditorParser : IGridEditorParser
+    {
+        /// <summary>
+        /// Check if it is a parser for a speficic editor
+        /// </summary>
+        /// <param name="editorview">
+        /// The editor view alias as defined in grid config
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool IsParserFor(string editorview)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the linked entities for this editor
+        /// </summary>
+        /// <param name="value">
+        /// The grid editor value
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(string value)
+        {
+            return Enumerable.Empty<ILinkedEntity>();
+        }
+    }
+}
