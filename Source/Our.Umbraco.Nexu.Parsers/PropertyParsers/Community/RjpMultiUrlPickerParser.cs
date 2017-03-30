be97f5de69edde9084e973c23010d502245880f8
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-30 16:50:11 +0200

    Added parser for RJP multi url picker

---
+++ Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Community/RjpMultiUrlPickerParser.cs
@@ -0,0 +1,43 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.PropertyParsers.Community
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using global::Umbraco.Core.Models;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+
+    /// <summary>
+    /// The rjp multi url picker parser.
+    /// </summary>
+    public class RjpMultiUrlPickerParser : IPropertyParser
+    {
+        /// <summary>
+        /// Check if it's a parser for a data type definition
+        /// </summary>
+        /// <param name="dataTypeDefinition">
+        /// The data type definition.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the linked entites from the property value
+        /// </summary>
+        /// <param name="propertyValue">
+        /// The property value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(object propertyValue)
+        {
+            return Enumerable.Empty<ILinkedEntity>();
+        }
+    }
+}
