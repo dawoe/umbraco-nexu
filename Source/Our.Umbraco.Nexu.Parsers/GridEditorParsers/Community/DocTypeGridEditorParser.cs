bec3edd3c8e6f595238ce686012549014521555a
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-30 08:27:52 +0200

    Setup unit test for GetLinkedEntities for DocTypeGridEditorParser

--- Source/Our.Umbraco.Nexu.Parsers/GridEditorParsers/Community/DocTypeGridEditorParser.cs
+++ Source/Our.Umbraco.Nexu.Parsers/GridEditorParsers/Community/DocTypeGridEditorParser.cs
@@ -3,12 +3,50 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Services;
+
     using Our.Umbraco.Nexu.Core.Interfaces;
+
     /// <summary>
     /// The doc type grid editor parser.
     /// </summary>
     public class DocTypeGridEditorParser : IGridEditorParser
     {
+        /// <summary>
+        /// The content type service.
+        /// </summary>
+        private readonly IContentTypeService contentTypeService;
+
+        /// <summary>
+        /// The data type service.
+        /// </summary>
+        private readonly IDataTypeService dataTypeService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocTypeGridEditorParser"/> class.
+        /// </summary>
+        public DocTypeGridEditorParser()
+        {
+            this.contentTypeService = ApplicationContext.Current.Services.ContentTypeService;
+            this.dataTypeService = ApplicationContext.Current.Services.DataTypeService;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocTypeGridEditorParser"/> class.
+        /// </summary>
+        /// <param name="contentTypeService">
+        /// The content type service.
+        /// </param>
+        /// <param name="dataTypeService">
+        /// The data type service.
+        /// </param>
+        public DocTypeGridEditorParser(IContentTypeService contentTypeService, IDataTypeService dataTypeService)
+        {
+            this.contentTypeService = contentTypeService;
+            this.dataTypeService = dataTypeService;
+        }
+
         /// <summary>
         /// Check if it is a parser for a speficic editor
         /// </summary>
