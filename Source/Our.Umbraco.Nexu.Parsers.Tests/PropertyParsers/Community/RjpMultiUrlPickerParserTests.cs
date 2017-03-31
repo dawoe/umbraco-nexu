77486f64a7de6e5cbfe6a22816b5728795aaa62b
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-03-31 11:52:14 +0200

    Added unit tests for IsParserFor method of RjpMultiUrlPickerParser

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/PropertyParsers/Community/RjpMultiUrlPickerParserTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.PropertyParsers.Community
+{
+    using global::Umbraco.Core.Models;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.PropertyParsers.Community;
+
+    /// <summary>
+    /// The rjp multi url picker parser tests.
+    /// </summary>
+    [TestFixture]
+    public class RjpMultiUrlPickerParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CommunityPropertyParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("RJP.MultiUrlPicker");
+
+            var parser = new RjpMultiUrlPickerParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CommunityPropertyParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new RjpMultiUrlPickerParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
