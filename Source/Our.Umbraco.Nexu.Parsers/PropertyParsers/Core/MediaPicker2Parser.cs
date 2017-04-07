7c7b973ee30cb81e0bda5c199438b875899263b6
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-04-07 07:53:07 +0200

    Added parser for new media picker in umbraco 7.6

---
+++ Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Core/MediaPicker2Parser.cs
@@ -0,0 +1,44 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.PropertyParsers.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using global::Umbraco.Core.Models;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+
+    /// <summary>
+    /// The media picker parser for sites using V7.6 and up
+    /// </summary>
+    public class MediaPicker2Parser : IPropertyParser
+    {
+        /// <summary>
+        /// Check if it's a parser for a data type definition
+        /// </summary>
+        /// <param name="dataTypeDefinition">
+        /// The data type definition.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the linked entites from the property value
+        /// </summary>
+        /// <param name="propertyValue">
+        /// The property value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(object propertyValue)
+        {
+            return Enumerable.Empty<ILinkedEntity>();
+        }
+    }
+}
