ca627aae7c08a48db71ce30c848cf8201f500d63
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-04-07 08:25:25 +0200

    Added unit tests for GetLinkedEntitiesMethod for MediaPicker2Parser

--- Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Core/MediaPicker2Parser.cs
+++ Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Core/MediaPicker2Parser.cs
@@ -4,7 +4,10 @@
     using System.Collections.Generic;
     using System.Linq;
 
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Cache;
     using global::Umbraco.Core.Models;
+    using global::Umbraco.Core.Services;
 
     using Our.Umbraco.Nexu.Core.Interfaces;
 
@@ -13,6 +16,40 @@
     /// </summary>
     public class MediaPicker2Parser : IPropertyParser
     {
+        /// <summary>
+        /// The media service.
+        /// </summary>
+        private readonly IMediaService mediaService;
+
+        /// <summary>
+        /// The cache provider.
+        /// </summary>
+        private readonly ICacheProvider cacheProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaPicker2Parser"/> class.
+        /// </summary>
+        public MediaPicker2Parser()
+        {
+            this.mediaService = ApplicationContext.Current.Services.MediaService;
+            this.cacheProvider = ApplicationContext.Current.ApplicationCache.StaticCache;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaPicker2Parser"/> class.
+        /// </summary>
+        /// <param name="mediaService">
+        /// The media service.
+        /// </param>
+        /// <param name="cacheProvider">
+        /// The cache provider.
+        /// </param>
+        public MediaPicker2Parser(IMediaService mediaService, ICacheProvider cacheProvider)
+        {
+            this.mediaService = mediaService;
+            this.cacheProvider = cacheProvider;
+        }
+
         /// <summary>
         /// Check if it's a parser for a data type definition
         /// </summary>
@@ -24,7 +61,7 @@
         /// </returns>
         public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
         {
-            return false;
+            return dataTypeDefinition.PropertyEditorAlias.Equals("Umbraco.MediaPicker2");
         }
 
         /// <summary>
