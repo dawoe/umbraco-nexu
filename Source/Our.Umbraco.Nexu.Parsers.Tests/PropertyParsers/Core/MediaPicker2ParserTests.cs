902668993734d17793f651200b443858a4fddaf4
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-04-07 07:59:13 +0200

    Added unit tests for IsParserFor method of MediaPicker2Parser

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/PropertyParsers/Core/MediaPicker2ParserTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.PropertyParsers.Core
+{
+    using global::Umbraco.Core.Models;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.PropertyParsers.Core;
+
+    /// <summary>
+    /// The media picker 2 parser tests.
+    /// </summary>
+    [TestFixture]
+    public class MediaPicker2ParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CorePropertyParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("Umbraco.MediaPicker2");
+
+            var parser = new MediaPicker2Parser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CorePropertyParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new MediaPicker2Parser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
