8789964de009b49807c697dc6ec80a7f4bd0d5db
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-04-08 11:53:02 +0200

    Added unit test for GetLinkedEntites of RichTextEditorParser for support in 7.6

--- Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Core/RichTextEditorParser.cs
+++ Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Core/RichTextEditorParser.cs
@@ -2,22 +2,64 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Text.RegularExpressions;
 
     using global::Umbraco.Core;
+    using global::Umbraco.Core.Cache;
     using global::Umbraco.Core.Logging;
     using global::Umbraco.Core.Models;
-
-    using HtmlAgilityPack;
+    using global::Umbraco.Core.Services;
 
     using Our.Umbraco.Nexu.Core.Interfaces;
-    using Our.Umbraco.Nexu.Core.Models;
 
     /// <summary>
     /// The rich text editor parser.
     /// </summary>
     public class RichTextEditorParser : IPropertyParser
     {
+        /// <summary>
+        /// The content service.
+        /// </summary>
+        private readonly IContentService contentService;
+
+        /// <summary>
+        /// The media service.
+        /// </summary>
+        private readonly IMediaService mediaService;
+
+        /// <summary>
+        /// The cache provider.
+        /// </summary>
+        private readonly ICacheProvider cacheProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RichTextEditorParser"/> class.
+        /// </summary>
+        public RichTextEditorParser()
+        {
+            this.contentService = ApplicationContext.Current.Services.ContentService;
+            this.mediaService = ApplicationContext.Current.Services.MediaService;
+            this.cacheProvider = ApplicationContext.Current.ApplicationCache.StaticCache;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RichTextEditorParser"/> class.
+        /// </summary>
+        /// <param name="contentService">
+        /// The content service.
+        /// </param>
+        /// <param name="mediaService">
+        /// The media service.
+        /// </param>
+        /// <param name="cacheProvider">
+        /// The cache provider.
+        /// </param>
+        public RichTextEditorParser(IContentService contentService, IMediaService mediaService, ICacheProvider cacheProvider)
+        {
+            this.contentService = contentService;
+            this.mediaService = mediaService;
+            this.cacheProvider = cacheProvider;
+        }
+
         /// <summary>
         /// Check if it's a parser for a data type definition
         /// </summary>
