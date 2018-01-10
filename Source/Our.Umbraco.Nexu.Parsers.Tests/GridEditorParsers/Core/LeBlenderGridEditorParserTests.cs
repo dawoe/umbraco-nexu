ef8a02a28f44bda893b5f1f26610f7e083257728
Mike Chambers <mistyn8@users.noreply.github.com>
2018-01-10 14:44:53 +0000

    Create LeBlenderGridEditorParserTests.cs
    Not having done any unit testing before, I've duped up the media one and provided a sample value...
    
    Not then sure how to mock content/media that has a udi?? But hopefully a starting point for you.

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/GridEditorParsers/Core/LeBlenderGridEditorParserTests.cs
@@ -0,0 +1,111 @@
+namespace Our.Umbraco.Nexu.Parsers.Tests.GridEditorParsers.Core
+{
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Core.Enums;
+    using Our.Umbraco.Nexu.Parsers.GridEditorParsers.Core;
+
+    /// <summary>
+    /// The media grid editor parser tests.
+    /// </summary>
+    [TestFixture]
+    public class LeBlenderGridEditorParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid view
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestIsParserForValidView()
+        {
+            // arrange            
+            var parser = new LeBlenderGridEditorParser();
+
+            // act
+            var result = parser.IsParserFor("/App_Plugins/LeBlender/editors/leblendereditor/LeBlendereditor.html");
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestIsParserForInValidView()
+        {
+            // arrange            
+            var parser = new LeBlenderGridEditorParser();
+
+            // act
+            var result = parser.IsParserFor("foo");
+
+            // verify
+            Assert.IsFalse(result);
+        }
+
+        /// <summary>
+        /// The test get linked entities with empty value.
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestGetLinkedEntitiesWithEmptyValue()
+        {
+            // arrange            
+            var parser = new LeBlenderGridEditorParser();
+
+            // act
+            var result = parser.GetLinkedEntities(null);
+
+            // verify
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(0, result.Count());
+        }
+
+        /// <summary>
+        /// The test get linked entities.
+        /// </summary>
+        [Test]
+        [Category("GridEditorParsers")]
+        [Category("CoreGridEditorParsers")]
+        public void TestGetLinkedEntities()
+        {
+            // arrange            
+            var parser = new LeBlenderGridEditorParser();
+
+            string value = @" [
+  {
+    ""nodeId"": {
+      ""value"": ""umb://document/680f252a8501444b840715302132c53c"",
+      ""dataTypeGuid"": ""fd1e0da5-5606-4862-b679-5d0cf3a52a59"",
+      ""editorAlias"": ""nodeId"",
+      ""editorName"": ""Content Page""
+    },
+    ""themePicker"": {
+      ""value"": ""31"",
+      ""dataTypeGuid"": ""395625fb-05b7-47b8-aace-eb57052de203"",
+      ""editorAlias"": ""themePicker"",
+      ""editorName"": ""Theme Picker""
+    }
+  }
+]";
+
+            // act
+            var result = parser.GetLinkedEntities(value).ToList();
+
+            // verify
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(1, result.Count());
+
+            Assert.IsTrue(result.Any(x => x.LinkedEntityType == LinkedEntityType.Content && x.Id == ???));
+        }
+    }
+}
