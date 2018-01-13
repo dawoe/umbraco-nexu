9ee5cc950f4debda3ca3025b3f2874b8e2bae5a4
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-01-13 11:16:03 +0100

    Added url of new api method to server variables

--- Source/Our.Umbraco.Nexu.Core/BootStrapper.cs
+++ Source/Our.Umbraco.Nexu.Core/BootStrapper.cs
@@ -98,6 +98,7 @@
 
             urlDictionairy.Add("BaseApi", urlHelper.GetUmbracoApiServiceBaseUrl<NexuApiController>(c => c.GetIncomingLinks(-1)));
             urlDictionairy.Add("GetIncomingLinks", urlHelper.GetUmbracoApiService<NexuApiController>("GetIncomingLinks", null));
+            urlDictionairy.Add("CheckDescendantsForIncomingLinks", urlHelper.GetUmbracoApiService<NexuApiController>("CheckDescendantsForIncomingLinks", null));
             urlDictionairy.Add("GetRebuildStatus", urlHelper.GetUmbracoApiService<NexuApiController>("GetRebuildStatus"));
             urlDictionairy.Add("Rebuild", urlHelper.GetUmbracoApiService<NexuApiController>("Rebuild", null));
 
