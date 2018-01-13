0cbd7bdaf8b8e8557dfe205142b20fa230e466a5
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-01-13 11:38:46 +0100

    Added unit tests for first level check of media items for incoming links

--- Source/Our.Umbraco.Nexu.Core.Tests/NexuApiControllerTests.cs
+++ Source/Our.Umbraco.Nexu.Core.Tests/NexuApiControllerTests.cs
@@ -207,7 +207,7 @@
         /// </summary>
         [Test]
         [Category("Api")]
-        public void CheckDescendantsForIncomingLinksShouldReturnFalseWhenThereAreNoChildren()
+        public void CheckContentDescendantsForIncomingLinksShouldReturnFalseWhenThereAreNoChildren()
         {
             // arrange
             var contentId = 123;
@@ -235,7 +235,7 @@
         /// </summary>
         [Test]
         [Category("Api")]
-        public void CheckDescendantsForIncomingLinksShouldReturnFalseWhenTheyHaveNoIncomingLinks()
+        public void CheckContentDescendantsForIncomingLinksShouldReturnFalseWhenTheyHaveNoIncomingLinks()
         {
             // arrange
             var contentId = 123;
@@ -273,7 +273,7 @@
         /// </summary>
         [Test]
         [Category("Api")]
-        public void CheckDescendantsForIncomingLinksShouldReturnTrueWhenOneHasIncomingLinks()
+        public void CheckContentDescendantsForIncomingLinksShouldReturnTrueWhenOneHasIncomingLinks()
         {
             // arrange
             var contentId = 123;
@@ -317,6 +317,129 @@
             Assert.IsTrue(result);
 
             this.contentServiceMock.Verify(x => x.GetChildren(contentId), Times.Once);
+
+            this.nexuServiceMock.Verify(x => x.GetNexuRelationsForContent(456, false), Times.Once);
+
+            this.nexuServiceMock.Verify(x => x.GetNexuRelationsForContent(000, false), Times.Once);
+
+            this.nexuServiceMock.Verify(x => x.GetNexuRelationsForContent(789, false), Times.Never);
+        }
+
+        /// <summary>
+        /// The check media descendants for incoming links should return false when there are no children.
+        /// </summary>
+        [Test]
+        [Category("Api")]
+        public void CheckMediaDescendantsForIncomingLinksShouldReturnFalseWhenThereAreNoChildren()
+        {
+            // arrange
+            var mediaId = 123;
+
+            var children = new List<IMedia>();
+
+            this.mediaServiceMock.Setup(x => x.GetChildren(mediaId)).Returns(children);
+
+            this.nexuServiceMock.Setup(x => x.GetNexuRelationsForContent(It.IsAny<int>(), false))
+                .Returns(new List<IRelation>());
+
+            // act
+            var result = this.controller.CheckMediaDescendantsForIncomingLinks(mediaId);
+
+            // arrange
+            Assert.IsFalse(result);
+
+            this.mediaServiceMock.Verify(x => x.GetChildren(mediaId), Times.Once);
+
+            this.nexuServiceMock.Verify(x => x.GetNexuRelationsForContent(It.IsAny<int>(), false), Times.Never);
+        }
+
+        /// <summary>
+        /// The check media descendants for incoming links should return false when they have no incoming links.
+        /// </summary>
+        [Test]
+        [Category("Api")]
+        public void CheckMediaDescendantsForIncomingLinksShouldReturnFalseWhenTheyHaveNoIncomingLinks()
+        {
+            // arrange
+            var mediaId = 123;
+
+            var children = new List<IMedia>();
+
+            var child1 = new Mock<IMedia>();
+            child1.Setup(x => x.Id).Returns(456);
+
+            children.Add(child1.Object);
+
+            var child2 = new Mock<IMedia>();
+            child2.Setup(x => x.Id).Returns(789);
+
+            children.Add(child2.Object);
+
+            this.mediaServiceMock.Setup(x => x.GetChildren(mediaId)).Returns(children);
+
+            this.nexuServiceMock.Setup(x => x.GetNexuRelationsForContent(It.IsAny<int>(), false))
+                .Returns(new List<IRelation>());
+
+            // act
+            var result = this.controller.CheckMediaDescendantsForIncomingLinks(mediaId);
+
+            // arrange
+            Assert.IsFalse(result);
+
+            this.mediaServiceMock.Verify(x => x.GetChildren(mediaId), Times.Once);
+
+            this.nexuServiceMock.Verify(x => x.GetNexuRelationsForContent(It.IsAny<int>(), false), Times.Exactly(children.Count));
+        }
+
+        /// <summary>
+        /// The check media descendants for incoming links should return true when one has incoming links.
+        /// </summary>
+        [Test]
+        [Category("Api")]
+        public void CheckMediaDescendantsForIncomingLinksShouldReturnTrueWhenOneHasIncomingLinks()
+        {
+            // arrange
+            var mediaId = 123;
+
+            var children = new List<IMedia>();
+
+            var child1 = new Mock<IMedia>();
+            child1.Setup(x => x.Id).Returns(456);
+
+            children.Add(child1.Object);
+
+            var child2 = new Mock<IMedia>();
+            child2.Setup(x => x.Id).Returns(000);
+
+            children.Add(child2.Object);
+
+            var child3 = new Mock<IMedia>();
+            child3.Setup(x => x.Id).Returns(789);
+
+            children.Add(child3.Object);
+
+            this.mediaServiceMock.Setup(x => x.GetChildren(mediaId)).Returns(children);
+
+            var relations = new List<IRelation>();
+
+            relations.Add(Mock.Of<IRelation>());
+
+            this.nexuServiceMock.Setup(x => x.GetNexuRelationsForContent(456, false))
+                .Returns(new List<IRelation>());
+
+            this.nexuServiceMock.Setup(x => x.GetNexuRelationsForContent(000, false))
+                .Returns(relations);
+
+            this.nexuServiceMock.Setup(x => x.GetNexuRelationsForContent(789, false))
+                .Returns(new List<IRelation>());
+
+            // act
+            var result = this.controller.CheckMediaDescendantsForIncomingLinks(mediaId);
+
+            // arrange
+            Assert.IsTrue(result);
+
+            this.mediaServiceMock.Verify(x => x.GetChildren(mediaId), Times.Once);
 
             this.nexuServiceMock.Verify(x => x.GetNexuRelationsForContent(456, false), Times.Once);
 
