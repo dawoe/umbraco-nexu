8bb7457f19dc8c5bfb8091958e707e246d1a1c56
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-03-19 20:49:18 +0100

    Basic version of property editor

--- Source/Our.Umbraco.Nexu.Core/Client/controllers/property-editor-controller.js
+++ Source/Our.Umbraco.Nexu.Core/Client/controllers/property-editor-controller.js
@@ -1,5 +1,13 @@
 ﻿angular.module('umbraco').controller('Our.Umbraco.Nexu.PropertyEditorController',
-    ['$scope', 'Our.Umbraco.Nexu.Resource',
-        function ($scope, nexuResource) {
+    ['$scope', '$routeParams', 'Our.Umbraco.Nexu.Resource',
+        function ($scope, $routeParams, nexuResource) {
+           
 
+            function init() {
+                nexuResource.getIncomingLinks($routeParams.id).then(function (result) {
+                    $scope.links = result.data;                    
+                });
+            }
+
+            init();
         }]);
