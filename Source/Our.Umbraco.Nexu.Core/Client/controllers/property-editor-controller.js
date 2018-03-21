d2f58215b0b106c1dcaa7159a74c8f3903a73375
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-03-21 11:11:43 +0100

    #10 don't load relations in create mode

--- Source/Our.Umbraco.Nexu.Core/Client/controllers/property-editor-controller.js
+++ Source/Our.Umbraco.Nexu.Core/Client/controllers/property-editor-controller.js
@@ -1,12 +1,22 @@
 ﻿angular.module('umbraco').controller('Our.Umbraco.Nexu.PropertyEditorController',
     ['$scope', '$routeParams', 'Our.Umbraco.Nexu.Resource',
         function ($scope, $routeParams, nexuResource) {
-           
+
+            $scope.isCreate = false;
+
+            if ($routeParams.create) {
+                $scope.isCreate = true;
+            }
 
             function init() {
-                nexuResource.getIncomingLinks($routeParams.id).then(function (result) {
-                    $scope.links = result.data;                    
-                });
+
+                if ($scope.isCreate === false) {
+                    nexuResource.getIncomingLinks($routeParams.id).then(function (result) {
+                        $scope.links = result.data;
+                    });
+                }
+                
+
             }
 
             init();
