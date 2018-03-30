00bd01d9410f4ea3bd08680efbb09508a7c11a78
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-03-30 08:58:41 +0200

    #22 read settings from appsettings for allowing delete and unpublish

---
+++ Source/Our.Umbraco.Nexu.Core/Constants/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Our.Umbraco.Nexu.Core.Constants
+{
+    /// <summary>
+    /// The app settings constants
+    /// </summary>
+    internal class AppSettings
+    {
+        /// <summary>
+        /// Allow delete app setting
+        /// </summary>
+        public const string AllowDelete = "nexu:AllowDelete";
+
+        /// <summary>
+        /// Allow unpublish app setting
+        /// </summary>
+        public const string AllowUnPublish = "nexu:AllowUnPublish";
+    }
+}
