f155139432597ffba60eabd365c230631d2e61d7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-04-03 17:29:46 +0200

    #23 added unit test and implemented method for gettig linked entities from inner content

--- Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Community/InnerContentParser.cs
+++ Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Community/InnerContentParser.cs
@@ -1,13 +1,19 @@
 ﻿namespace Our.Umbraco.Nexu.Parsers.PropertyParsers.Community
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
     using global::Umbraco.Core;
+    using global::Umbraco.Core.Logging;
     using global::Umbraco.Core.Models;
     using global::Umbraco.Core.Services;
 
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
     using Our.Umbraco.Nexu.Core.Interfaces;
+    using Our.Umbraco.Nexu.Core.ObjectResolution;
 
     /// <summary>
     /// The inner content parser.
@@ -80,7 +86,81 @@
 
             var entities = new List<ILinkedEntity>();
 
+            var contentTypes = new Dictionary<string, IContentType>();
+            var dataTypes = new Dictionary<int, IDataTypeDefinition>();
+
+            try
+            {
+                var items = JsonConvert.DeserializeObject<List<object>>(propertyValue.ToString());
+
+                for (var i = 0; i < items.Count; i++)
+                {
+                    var item = (JObject)items[i];
+
+                    var docTypeGuid = this.GetDocTypeGuidFromItem(item);
+
+                    if (!string.IsNullOrEmpty(docTypeGuid))
+                    {
+                        if (!contentTypes.ContainsKey(docTypeGuid))
+                        {
+                            contentTypes.Add(
+                                docTypeGuid,
+                                this.contentTypeService.GetContentType(new Guid(docTypeGuid)));
+                        }
+
+                        var doctype = contentTypes[docTypeGuid];
+
+                        // get all datatypes for this content type
+                        foreach (var propertyType in doctype.PropertyTypes)
+                        {
+                            if (!dataTypes.ContainsKey(propertyType.DataTypeDefinitionId))
+                            {
+                                // retreive datatype and stash it in dictionary
+                                dataTypes.Add(
+                                    propertyType.DataTypeDefinitionId,
+                                    this.dataTypeService.GetDataTypeDefinitionById(propertyType.DataTypeDefinitionId));
+                            }
+
+                            var dtd = dataTypes[propertyType.DataTypeDefinitionId];
+
+                            // get the parser for the current property
+                            var parser = PropertyParserResolver.Current.Parsers.FirstOrDefault(x => x.IsParserFor(dtd));
+
+                            if (parser != null)
+                            {
+                                var propValue = this.GetPropertyValueFromItem(item, propertyType.Alias);
+
+                                if (propValue != null)
+                                {
+                                    entities.AddRange(parser.GetLinkedEntities(propValue));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                ApplicationContext.Current.ProfilingLogger.Logger.Error<InnerContentParser>(
+                    "Error parsing stacked content",
+                    exception);
+            }
+
             return entities;
+        }
+
+        private string GetDocTypeGuidFromItem(JObject item)
+        {
+            var guidProperty = item["icContentTypeGuid"];
+
+            return guidProperty?.ToObject<string>();
+        }
+
+        private string GetPropertyValueFromItem(JObject item, string propertyAlias)
+        {
+            var property = item[propertyAlias];
+
+            return property?.ToString();
         }
     }
 }
