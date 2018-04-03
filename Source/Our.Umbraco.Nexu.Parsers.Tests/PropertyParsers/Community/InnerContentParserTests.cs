3de337b04e04db09347b284d5c24e9f4e2301a72
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-04-03 16:42:37 +0200

    #23 added unit tests for is parser for method of innercontent datatypes

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/PropertyParsers/Community/InnerContentParserTests.cs
@@ -0,0 +1,55 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.PropertyParsers.Community
+{
+    using global::Umbraco.Core.Models;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Parsers.PropertyParsers.Community;
+
+    /// <summary>
+    /// The inner content parser tests.
+    /// </summary>
+    [TestFixture]
+    public class InnerContentParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CommunityPropertyParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("Our.Umbraco.StackedContent");
+
+            var parser = new InnerContentParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CommunityPropertyParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new InnerContentParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
