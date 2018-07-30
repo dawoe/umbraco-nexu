744db18e07d9b5bebaaeb4d71197b8910e430966
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-07-30 08:16:04 +0200

    #33 added parser and unit test for content list property editor

---
+++ Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Community/ContentListParser.cs
@@ -0,0 +1,33 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.PropertyParsers.Community
+{
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Services;
+
+    /// <summary>
+    /// The content list parser.
+    /// </summary>
+    public class ContentListParser : StackedContentParser
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentListParser"/> class.
+        /// </summary>
+        public ContentListParser()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContentListParser"/> class.
+        /// </summary>
+        /// <param name="contentTypeService">
+        /// The content type service.
+        /// </param>
+        /// <param name="dataTypeService">
+        /// The data Type Service.
+        /// </param>
+        public ContentListParser(IContentTypeService contentTypeService, IDataTypeService dataTypeService)
+            : base(contentTypeService, dataTypeService)
+        {
+        }
+    }
+}
