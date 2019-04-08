39e72a2de1c7695aa9a79c99142b02237225e6ac
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-04-08 08:04:22 +0200

    Removed old source code

--- Source/Our.Umbraco.Nexu.Core/Client/controllers/media-delete-controller.js
+++
@@ -1,9 +0,0 @@
-﻿angular.module('umbraco').controller('Our.Umbraco.Nexu.MediaDeleteController',
-    ['$scope', '$controller', 
-    function ($scope, $controller) {
-        $scope.isMedia = true;
-
-        // inherit base delete controller        
-        angular.extend(this, $controller('Our.Umbraco.Nexu.BaseDeleteController', { $scope: $scope }));
-        
-    }]);
--- Source/Our.Umbraco.Nexu.Core/Client/directives/nexuDescendantsWarning.js
+++
@@ -1,9 +0,0 @@
-﻿angular.module("umbraco.directives")
-    .directive('nexuDescendantsWarning', function () {
-        return {
-            restrict: "E",    // restrict to an element
-            replace: true,   // replace the html element with the template
-            transclude: true,
-            templateUrl: '/App_Plugins/Nexu/views/nexu-descendants-warning.html'            
-        };
-    });
