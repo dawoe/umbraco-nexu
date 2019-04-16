053edb8c549f103b0b9a577bf0d1a56383d50c58
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-04-16 17:39:21 +0200

    Added property parser interface

---
+++ src/Our.Umbraco.Nexu.Common/Interfaces/Models/IPropertyValueParser.cs
@@ -0,0 +1,34 @@
+﻿namespace Our.Umbraco.Nexu.Common.Interfaces.Models
+{
+    using System.Collections.Generic;
+
+    using global::Umbraco.Core.Models;
+
+    /// <summary>
+    /// The PropertyValueParser interface.
+    /// </summary>
+    public interface IPropertyValueParser
+    {
+        /// <summary>
+        /// Checks if this is the parser for the current property
+        /// </summary>
+        /// <param name="property">
+        /// The property.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        bool IsParserFor(Property property);
+
+        /// <summary>
+        /// Gets the related entities from the property value
+        /// </summary>
+        /// <param name="property">
+        /// The property.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable{IRelatedEntity}"/>.
+        /// </returns>
+        IEnumerable<IRelatedEntity> GetRelatedEntities(Property property);
+    }
+}
