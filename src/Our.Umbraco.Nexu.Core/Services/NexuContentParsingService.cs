499f1f9d019196766c1ed5e734eb4e73fcbb6e85
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-04-24 09:04:10 +0200

    Added nexu enitty parsing service

---
+++ src/Our.Umbraco.Nexu.Core/Services/NexuContentParsingService.cs
@@ -0,0 +1,17 @@
+﻿namespace Our.Umbraco.Nexu.Core.Services
+{
+    using global::Umbraco.Core.Models;
+
+    using Our.Umbraco.Nexu.Common.Interfaces.Services;
+
+    /// <summary>
+    /// Represents the nexu entity parsing service
+    /// </summary>
+    internal class NexuEntityParsingService : IEntityParsingService
+    {
+        public void ParseContent(IContent content)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
