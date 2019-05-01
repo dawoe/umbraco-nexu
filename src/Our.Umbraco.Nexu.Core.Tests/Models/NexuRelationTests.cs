b28eefed00b61ed586ba05058d700df785029ce8
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-05-01 10:49:21 +0200

    Added model for nexu relations

---
+++ src/Our.Umbraco.Nexu.Core.Tests/Models/NexuRelationTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Our.Umbraco.Nexu.Core.Tests.Models
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Core.Models;
+
+    /// <summary>
+    /// Represents nexu relation model tests.
+    /// </summary>
+    [TestFixture]
+    public class NexuRelationTests
+    {
+        [Test]
+        public void When_NexuRelation_Instance_Is_Created_Id_Should_Be_A_Guid()
+        {
+            // act
+            var result = new NexuRelation();
+
+            // assert
+            Assert.That(result.Id != Guid.Empty);
+        }
+
+        [Test]
+        public void Nexu_Relation_Udi_Should_Match_Id()
+        {
+            // act
+            var result = new NexuRelation();
+
+            // assert
+            Assert.That(result.Udi.EntityType == "nexurelation");
+            Assert.That(result.Udi.ToString() == $"umb://nexurelation/{result.Id.ToString("N")}");
+        }
+    }
+}
---
+++ src/Our.Umbraco.Nexu.Core/Models/NexuRelation.cs
@@ -0,0 +1,65 @@
+﻿namespace Our.Umbraco.Nexu.Core.Models
+{
+    using System;
+
+    using global::Umbraco.Core;
+
+    /// <summary>
+    /// Represents a nexu relation
+    /// </summary>
+    internal class NexuRelation
+    {
+        private Udi udi;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NexuRelation"/> class.
+        /// </summary>
+        public NexuRelation()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parent udi
+        /// </summary>
+        public Udi Parent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the child udi
+        /// </summary>
+        public Udi Child { get; set; }
+
+        /// <summary>
+        /// Gets or sets the relation type.
+        /// </summary>
+        public Guid RelationType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the property id.
+        /// </summary>
+        public int PropertyId { get; set; }
+
+        /// <summary>
+        /// Gets the udi.
+        /// </summary>
+        public Udi Udi
+        {
+            get
+            {
+                if (this.udi != null)
+                {
+                    return this.udi;
+                }
+
+                this.udi = new GuidUdi("nexurelation", this.Id);
+
+                return this.udi;
+            }
+        }
+    }
+}
