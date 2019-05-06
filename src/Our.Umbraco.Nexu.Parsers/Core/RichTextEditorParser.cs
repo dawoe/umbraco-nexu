1548bbdf605cbc82b27ded49927c74fbc116b112
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-05-06 17:02:13 +0200

    Refactored so we have a base text parser for properties with "rich" content

--- src/Our.Umbraco.Nexu.Parsers/Core/RichTextEditorParser.cs
+++ src/Our.Umbraco.Nexu.Parsers/Core/RichTextEditorParser.cs
@@ -1,52 +1,16 @@
 ﻿namespace Our.Umbraco.Nexu.Parsers.Core
 {
-    using System.Collections.Generic;
-    using System.Linq;
-
     using global::Umbraco.Core;
-
-    using Our.Umbraco.Nexu.Common.Interfaces.Models;
-    using Our.Umbraco.Nexu.Common.Models;
-    using Our.Umbraco.Nexu.Parsers.Helpers;
 
     /// <summary>
     /// Represents the rich text editor parser
     /// </summary>
-    public class RichTextEditorParser : IPropertyValueParser
+    public class RichTextEditorParser : BaseTextParser
     {
         /// <inheritdoc />
-        public bool IsParserFor(string propertyEditorAlias)
+        public override bool IsParserFor(string propertyEditorAlias)
         {
             return propertyEditorAlias.Equals(Constants.PropertyEditors.Aliases.TinyMce);
-        }
-
-        /// <inheritdoc />
-        public IEnumerable<IRelatedEntity> GetRelatedEntities(string value)
-        {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                return Enumerable.Empty<IRelatedEntity>();
-            }
-
-            var relatedEntities = new List<IRelatedEntity>();          
-
-            foreach (var documentUdi in ParserUtilities.GetDocumentUdiFromText(value).ToList())
-            {
-                relatedEntities.Add(new RelatedDocumentEntity
-                                        {
-                                            RelatedEntityUdi = documentUdi
-                                        });
-            }
-
-            foreach (var mediaUdi in ParserUtilities.GetMediaUdiFromText(value).ToList())
-            {
-                relatedEntities.Add(new RelatedMediaEntity()
-                                        {
-                                            RelatedEntityUdi = mediaUdi
-                                        });
-            }
-
-            return relatedEntities;
-        }
+        }        
     }
 }
