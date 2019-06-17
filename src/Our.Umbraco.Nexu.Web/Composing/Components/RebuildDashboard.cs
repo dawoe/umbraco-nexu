d30f2d24882cf626f5474d9d7725fab1e0f24c80
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-06-17 08:15:26 +0200

    Added dashboard view and translation

--- src/Our.Umbraco.Nexu.Web/Composing/Components/RebuildDashboard.cs
+++ src/Our.Umbraco.Nexu.Web/Composing/Components/RebuildDashboard.cs
@@ -11,7 +11,7 @@
         public string Alias => "Our.Umbraco.Nexu.RebuildDashboard";
 
         /// <inheritdoc />
-        public string View => "~/App_Plugins/Nexu/views/dashboard.html";
+        public string View => "/App_Plugins/Nexu/views/dashboard.html";
 
         /// <inheritdoc />
         public string[] Sections => new string[] { new global::Umbraco.Web.Sections.SettingsSection().Alias};
