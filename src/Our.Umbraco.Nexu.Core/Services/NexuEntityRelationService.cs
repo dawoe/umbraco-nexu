15dcaa87b8cb7da868e0f93a6fff09faaed8f7d4
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-06-26 09:01:20 +0200

    Implement interface in concrete class

--- src/Our.Umbraco.Nexu.Core/Services/NexuEntityRelationService.cs
+++ src/Our.Umbraco.Nexu.Core/Services/NexuEntityRelationService.cs
@@ -1,9 +1,11 @@
 ﻿namespace Our.Umbraco.Nexu.Core.Services
 {
+    using Our.Umbraco.Nexu.Common.Interfaces.Services;
+
     /// <summary>
     /// Represents nexu entity relation service.
     /// </summary>
-    public class NexuEntityRelationService
+    public class NexuEntityRelationService : IEntityRelationService
     {
     }
 }
