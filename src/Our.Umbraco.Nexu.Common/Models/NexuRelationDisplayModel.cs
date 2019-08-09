93a14828708b8675e74f55f39511cfaf37b31d1c
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-08-09 14:35:50 +0200

    Added models for properties

--- src/Our.Umbraco.Nexu.Common/Models/NexuRelationDisplayModel.cs
+++ src/Our.Umbraco.Nexu.Common/Models/NexuRelationDisplayModel.cs
@@ -1,12 +1,21 @@
 ﻿namespace Our.Umbraco.Nexu.Common.Models
 {
     using System;
+    using System.Collections.Generic;
 
     /// <summary>
     /// Represents the nexu relation display model.
     /// </summary>
     public class NexuRelationDisplayModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NexuRelationDisplayModel"/> class.
+        /// </summary>
+        public NexuRelationDisplayModel()
+        {
+            this.Properties = new List<NexuRelationPropertyDisplay>();
+        }
+
         /// <summary>
         /// Gets or sets the id.
         /// </summary>
@@ -36,5 +45,10 @@
         /// Gets or sets a value indicating whether is trashed.
         /// </summary>
         public bool IsTrashed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the properties.
+        /// </summary>
+        public IList<NexuRelationPropertyDisplay> Properties { get; set; }
     }
 }
---
+++ src/Our.Umbraco.Nexu.Common/Models/NexuRelationPropertyDisplay.cs
@@ -0,0 +1,18 @@
+﻿namespace Our.Umbraco.Nexu.Common.Models
+{
+    /// <summary>
+    /// Represents the  nexu relation property display model
+    /// </summary>
+    public class NexuRelationPropertyDisplay
+    {
+        /// <summary>
+        /// Gets or sets the property name.
+        /// </summary>
+        public string PropertyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tab name.
+        /// </summary>
+        public string TabName { get; set; }
+    }
+}
