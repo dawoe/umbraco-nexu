d63e6d1afdc12cf81bf2b5f1af3283fc8db59036
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-09-02 08:22:40 +0200

    Added relation check angular resource

---
+++ src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/resources/relation-check-resource.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    function RelationCheckResource($http, umbRequestHelper) {
+
+        var apiUrl = Umbraco.Sys.ServerVariables.Nexu.RelationCheckApi;
+
+        var resource = {
+            getIncomingLinks: getIncomingLinks
+        };
+
+        return resource;
+
+        function getIncomingLinks(udi) {
+
+            return umbRequestHelper.resourcePromise(
+                $http.get(apiUrl + 'GetIncomingLinks?udi=' + udi),
+                'Failed to get incoming links'
+            );
+        };
+
+    }
+
+    angular.module('umbraco.resources').factory('Our.Umbraco.Nexu.Resources.RelationCheckResource', RelationCheckResource);
+
+})();
