1ad2fc10f36697192e275642e3c6d7efee3a8d27
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-09-02 08:28:14 +0200

    Added relation check service

---
+++ src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/services/relation-check-service.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    'use strict';
+
+    function RelationCheckServive($q, resource) {
+
+        var service = {
+            checkRelations: checkRelations
+        };
+
+        return service;
+
+        function getIncheckRelationscomingLinks(udi) {
+
+            var deferred = $q.defer();
+
+            resource.getIncomingLinks(udi).then(function (data) {
+                    deferred.resolve(data);
+                },
+                function () {
+                    deferred.reject();
+                });
+
+            return deferred.promise;
+        };
+
+    }
+
+    angular.module('umbraco.resources').factory('Our.Umbraco.Nexu.Services.RelationCheckServive',
+        ['$q', 'Our.Umbraco.Nexu.Resources.RelationCheckResource', RelationCheckServive]
+        );
+
+})();
