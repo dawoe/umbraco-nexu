cc5970b3e48b56cfce40617616794b8c6ff1b96f
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-09-02 08:00:17 +0200

    Refactored so the related links list is a component

---
+++ src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/components/relation-list-component.js
@@ -0,0 +1,50 @@
+﻿(function () {
+	'use strict';
+
+	function RelationListComponentController($scope, $location) {
+		var vm = this;
+		
+		vm.ungrouped = [];
+
+        vm.openContent = function (link) {
+            $location.url(link);
+        }
+
+        this.$onInit= function() {
+            for (var i = 0; i < this.relations.length; i++) {
+                var relation = this.relations[i];
+
+                for (var j = 0; j < relation.Properties.length; j++) {
+                    var status = 'Unpublished';
+
+                    if (relation.IsTrashed) {
+                        status = 'Recycle bin';
+                    }
+                    else if (relation.IsPublished) {
+                        status = 'Published';
+                    }
+
+                    vm.ungrouped.push({
+                        name: relation.Name,
+                        propertyname: relation.Properties[j].PropertyName,
+                        tabname: relation.Properties[j].TabName,
+                        status: status,
+                        editlink: '/content/content/edit/' + relation.Id + '?mculture=' + relation.Culture
+                    });
+                }
+            }
+        }
+
+    }
+
+	angular.module('umbraco').component('nexuRelationList',
+        {
+            controller: RelationListComponentController,
+        	controllerAs: "vm",
+            bindings: {
+                relations: '<'
+            },
+            templateUrl: '/App_Plugins/Nexu/views/relation-list-component.html'
+        });
+
+})();
--- src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/controllers/related-links-app-controller.js
+++ src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/controllers/related-links-app-controller.js
@@ -14,37 +14,7 @@
         } else {
             vm.cultureRelations = vm.relations;
         }
-      
-        
 
-        vm.ungrouped = [];
-
-        for (var i = 0; i < vm.cultureRelations.length; i++) {
-            var relation = vm.cultureRelations[i];
-            
-            for (var j = 0; j < relation.Properties.length; j++) {
-                var status = 'Unpublished';
-
-                if (relation.IsTrashed) {
-                    status = 'Recycle bin';
-                }
-                else if (relation.IsPublished) {
-                    status = 'Published';
-                }
-
-                vm.ungrouped.push({
-                    name: relation.Name,
-                    propertyname: relation.Properties[j].PropertyName,
-                    tabname: relation.Properties[j].TabName,
-                    status : status,
-                    editlink: '/content/content/edit/' + relation.Id + '?mculture=' + relation.Culture
-                });
-            }
-        }
-
-        vm.openContent = function(link) {
-            $location.url(link);
-        }
     }
 
     angular.module('umbraco').controller('Our.Umbraco.Nexu.Controllers.RelatedLinksAppController',
