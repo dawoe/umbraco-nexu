2a3f0e8e6c46a77228cbe34b3370947141eb06d5
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-09-04 08:58:53 +0200

    Disable submit button while loading relations in unpublish dialog

--- src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/controllers/unpublish-controller.js
+++ src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/controllers/unpublish-controller.js
@@ -7,9 +7,12 @@
 
         vm.loading = true;
         vm.relations = [];
+        vm.showLanguageColumn = false;
+        $scope.model.disableSubmitButton = true;
 
         function init() {
-            service.checkRelations(editorState.current.udi).then(function(data) {
+            service.checkRelations(editorState.current.udi).then(function (data) {
+                $scope.model.disableSubmitButton = false;
                 vm.relations = data;
                 vm.loading = false;
             });
