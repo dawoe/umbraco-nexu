9c1220fc58ecbe48ec0d8af7079a0a5f46619fd9
Rachel Breeze <Rachel.Breeze@corp.sigmauk>
2020-01-21 22:38:21 +0000

    This change is to allow Nexu to Parse the Social Image Media property of SEO Checker (https://soetemansoftware.nl/seo-checker). It has been tested against: - Umbraco 7.13.2 - SEOChecker 2.2.4 - ClientDependency 1.9.8 - ClientDependency-Mvc5 1.9.3

---
+++ Source/Our.Umbraco.Nexu.Parsers.Tests/PropertyParsers/Community/SEOCheckerParserTests.cs
@@ -0,0 +1,63 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.Tests.PropertyParsers.Community
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using global::Umbraco.Core.Models;
+    using global::Umbraco.Core.Services;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using Our.Umbraco.Nexu.Core.Enums;
+    using Our.Umbraco.Nexu.Parsers.PropertyParsers.Community;
+
+    /// <summary>
+    /// The inner content parser tests.
+    /// </summary>
+    [TestFixture]
+    public class SEOCheckerParserTests : BaseParserTest
+    {
+        /// <summary>
+        /// The test is parser for valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CommunityPropertyParsers")]
+        public void TestIsParserForValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("SEOChecker.SEOCheckerSocialPropertyEditor");
+
+            var parser = new SEOCheckerParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// The test is parser for in valid data type.
+        /// </summary>
+        [Test]
+        [Category("PropertyParsers")]
+        [Category("CommunityPropertyParsers")]
+        public void TestIsParserForInValidDataType()
+        {
+            // arrange
+            var dataTypeDefinition = new DataTypeDefinition("foo");
+
+            var parser = new SEOCheckerParser();
+
+            // act
+            var result = parser.IsParserFor(dataTypeDefinition);
+
+            // verify
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ Source/Our.Umbraco.Nexu.Parsers/PropertyParsers/Community/SEOCheckerParser.cs
@@ -0,0 +1,83 @@
+﻿namespace Our.Umbraco.Nexu.Parsers.PropertyParsers.Community
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Logging;
+    using global::Umbraco.Core.Models;
+    using global::Umbraco.Core.Services;
+    using global::Umbraco.Core.Cache;
+
+    using Our.Umbraco.Nexu.Core.Interfaces;
+    using Our.Umbraco.Nexu.Core.Models;
+    public class SEOCheckerParser : IPropertyParser
+    {
+        /// <summary>
+        /// The media service.
+        /// </summary>
+        private readonly IMediaService mediaService;
+
+        /// <summary>
+        /// The cache provider.
+        /// </summary>
+        private readonly ICacheProvider cacheProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SEOCheckerParser"/> class.
+        /// </summary>
+        public SEOCheckerParser()
+        {
+            this.mediaService = ApplicationContext.Current.Services.MediaService;
+            this.cacheProvider = ApplicationContext.Current.ApplicationCache.StaticCache;
+        }
+
+        /// <inheritdoc />
+        public bool IsParserFor(IDataTypeDefinition dataTypeDefinition)
+        {
+            return dataTypeDefinition.PropertyEditorAlias.Equals("SEOChecker.SEOCheckerSocialPropertyEditor");
+        }
+        /// <inheritdoc />
+        public IEnumerable<ILinkedEntity> GetLinkedEntities(object propertyValue)
+        {
+            if (string.IsNullOrEmpty(propertyValue?.ToString()))
+            {
+                return Enumerable.Empty<ILinkedEntity>();
+            }
+            var entities = new List<ILinkedEntity>();
+            var model = XDocument.Parse(propertyValue.ToString()).Root;
+            if (model != null)
+            {
+                try
+                {
+                    var socialImage = model.Descendants("socialImage").FirstOrDefault();
+                    if (socialImage == null)
+                    {
+                        return Enumerable.Empty<ILinkedEntity>();
+                    }
+
+                    if (string.IsNullOrEmpty(socialImage.Value))
+                    {
+                        return Enumerable.Empty<ILinkedEntity>();
+                    }
+                     var id = ParserHelper.MapMediaUdiToId(this.mediaService, this.cacheProvider, socialImage.Value);
+
+                    if (id > -1)
+                    {
+                        entities.Add(new LinkedMediaEntity(id));
+                    }
+
+                    return entities;
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Error(this.GetType(), $"An error occured parsing SEO Checker property", ex);
+                }
+            }
+            return entities;
+        }
+        
+    }
+}
