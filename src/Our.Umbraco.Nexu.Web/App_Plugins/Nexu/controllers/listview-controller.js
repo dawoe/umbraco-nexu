66821be4c3b393583acd732c71d9ff7a89c98dcc
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2020-01-24 08:57:08 +0100

    Added custom listview controller that extends the default one

---
+++ src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/controllers/listview-controller.js
@@ -0,0 +1,9 @@
+﻿angular.module('umbraco').controller('Our.Umbraco.Nexu.ListViewController',
+    ['$scope', '$controller', 'Our.Umbraco.Nexu.Services.RelationCheckServive',
+        function ($scope, $controller, service) {
+            // inherit core delete controller
+            angular.extend(this, $controller('Umbraco.PropertyEditors.ListViewController', { $scope: $scope }));
+
+
+            
+        }]);
