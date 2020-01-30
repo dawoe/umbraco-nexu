fa32c25fdb9cb3cd0068da36946ee96c88648bf0
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2020-01-30 08:40:24 +0100

    Intercept delete of list view controller and only show old delete when there are no relations

--- src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/controllers/listview-controller.js
+++ src/Our.Umbraco.Nexu.Web/App_Plugins/Nexu/controllers/listview-controller.js
@@ -1,9 +1,46 @@
 ﻿angular.module('umbraco').controller('Our.Umbraco.Nexu.ListViewController',
-    ['$scope', '$controller', 'Our.Umbraco.Nexu.Services.RelationCheckServive',
-        function ($scope, $controller, service) {
+    ['$scope', '$controller', 'localizationService', 'overlayService', 'Our.Umbraco.Nexu.Resources.RelationCheckResource',
+        function ($scope, $controller, localizationService, overlayService, relationCheckResource) {
             // inherit core delete controller
             angular.extend(this, $controller('Umbraco.PropertyEditors.ListViewController', { $scope: $scope }));
 
+            oldDelete = $scope.delete;
 
+            $scope.delete = function () {
+
+                
+                relationCheckResource.checkLinkedItems($scope.selection).then(function(data) {
+                    if (data.length > 0) {
+
+                    } else {
+                        oldDelete();
+                    }
+                });
+
+               oldDelete();
+
+                //var dialog = {
+                //    view: "views/propertyeditors/listview/overlays/delete.html",
+                //    deletesVariants: selectionHasVariants(),
+                //    isTrashed: $scope.isTrashed,
+                //    submitButtonLabelKey: "contentTypeEditor_yesDelete",
+                //    submitButtonStyle: "danger",
+                //    submit: function (model) {
+                //        performDelete();
+                //        overlayService.close();
+                //    },
+                //    close: function () {
+                //        overlayService.close();
+                //    }
+                //};
+
+                //localizationService.localize("general_delete").then(value => {
+                //    dialog.title = value;
+                //    overlayService.open(dialog);
+                //});
+
+            };
+
+            
             
         }]);
